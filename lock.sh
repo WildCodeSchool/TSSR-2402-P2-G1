bab4235673296a92ccf611b6b80a28ecf29959c9
Ahmedbenrebai <161340742+Ahmedbenrebai@users.noreply.github.com>
2024-04-03 14:35:51 +0200

    Create lock.sh

---
+++ lock.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Fonction pour verrouiller un ordinateur client
+verrouiller_client() {
+    # Adresse IP ou nom d'hôte de l'ordinateur client
+    client_address="adresse_ip_ou_nom_hote"
+
+    # Nom d'utilisateur sur l'ordinateur client
+    username="utilisateur"
+
+    # Utilisation de la commande ssh pour se connecter à l'ordinateur client et verrouiller l'écran
+    ssh "$username@$client_address" "xlock"
+}
+
+# Appel de la fonction pour verrouiller l'ordinateur client
+verrouiller_client
