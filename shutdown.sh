5382c09cda61b090e3f9bd0d54f888d1644035a5
Ahmedbenrebai <161340742+Ahmedbenrebai@users.noreply.github.com>
2024-04-04 10:29:00 +0200

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -9,7 +9,7 @@ function redemarrer_client() {
     username="utilisateur"
 
     # Utilisation de la commande ssh pour redémarrer l'ordinateur distant
-    ssh "$username@$client_address" "sudo shutdown -r now"
+    ssh "$ssh wilder@172.16.10.30" "sudo shutdown -r now"
 }
 
 # Appel de la fonction pour redémarrer l'ordinateur distant
