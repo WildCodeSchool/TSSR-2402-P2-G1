0d1f8f7c1e403eb9c590b8cb1faf51dcf39c789e
cl_smoker <81968235+clsmoker88@users.noreply.github.com>
2024-04-04 09:25:06 +0200

    ajout de fonction call_ssh_EOF et ram_total

--- template.sh
+++ template.sh
@@ -7,11 +7,26 @@ function call_ssh()
     ssh -t $1@$2 $3
 }
 
+#fonction pour se connecter au ssh avec EOF
+function call_ssh_EOF
+{
+    ssh -t $1@$2
+}
+
 #fonction pour mettre a jour le systeme
 
 function update_system()
 {
     call_ssh $1 $2 "sudo apt update && sudo apt upgrade -y"
+}
+
+function ram_total()
+{
+    call_ssh_EOF $1 $2 <<-"EOF"
+    ram=$(free)
+    ram=$(echo $ram | cut -d' ' -f8)
+    echo $ram
+    EOF
 }
 
 read -p "quelle ordinateur voulez vous cibler? " choix_ordinateur
