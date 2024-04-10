2b9ad0e45da7642748e3cf1f7eee0980302f1591
cl_smoker <81968235+clsmoker88@users.noreply.github.com>
2024-04-10 14:34:20 +0200

    add function

---
+++ function.ps1
@@ -0,0 +1,44 @@
+﻿function update_system()
+{
+    Update-Help
+}
+
+function ram_total()
+{
+    Get-WmiObject Win32_PhysicalMemory | Format-Table capacity
+}
+
+function os_version()
+{
+    Get-WmiObject Win32_OperatingSystem | Format-Table Version
+}
+
+function add_user()
+{
+    
+    $username_user = Read-Host "quelle utilisateur voulez vous ajouter? "
+    if(Get-LocalUser -Name $username_user 2> $null)  
+    {
+        write-host "l'utilisateur existe deja"
+    }
+    else
+    {
+        $password_user = Read-Host -AsSecureString "choisissez un mot de passe pour le compte? "
+        New-LocalUser -Name $username_user -Password $password_user
+        if(Get-LocalUser -Name $username_user 2> $null)
+        {
+            write-host "l'utilisateur a bien été creer"
+        }
+    }
+
+}
+
+function list_user_local()
+{
+    Get-LocalUser
+}
+#update_system
+#ram_total
+#os_version
+#add_user
+list_user_local
