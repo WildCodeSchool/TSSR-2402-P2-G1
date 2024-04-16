b936e30e4b838490ab0f7c0ae7c047b7ea95e512
Ahmedbenrebai <161340742+Ahmedbenrebai@users.noreply.github.com>
2024-04-16 11:59:19 +0200

    Create poweroff.ps1

---
+++ poweroff.ps1
@@ -0,0 +1,19 @@
+# Fonction pour arrêter un ordinateur client
+function Arreter-Client {
+    # Adresse IP ou nom d'hôte de l'ordinateur client
+    $clientAddress = "adresse_ip_ou_nom_hote"
+
+    # Nom d'utilisateur sur l'ordinateur client
+    $username = "utilisateur"
+
+    $session = New-PSSession -ComputerName $clientAddress -Credential $username
+
+  
+    Invoke-Command -Session $session -ScriptBlock {
+        
+        Stop-Computer -Force
+    }
+
+    # Fermeture de la session PowerShell
+    Remove-PSSession $session
+}
