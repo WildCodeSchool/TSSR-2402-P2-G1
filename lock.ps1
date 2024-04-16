7200a53aa7a3a9c824b9f3d3021e079f4d367546
Ahmedbenrebai <161340742+Ahmedbenrebai@users.noreply.github.com>
2024-04-16 12:07:30 +0200

    Create lock.ps1

---
+++ lock.ps1
@@ -0,0 +1,17 @@
+function Verrouiller-Client {
+    # Adresse IP ou nom d'hôte de l'ordinateur client
+    $clientAddress = "adresse_ip_ou_nom_hote"
+
+    # Nom d'utilisateur sur l'ordinateur client
+    $username = "utilisateur"
+
+    $session = New-PSSession -ComputerName $clientAddress -Credential $username
+
+    Invoke-Command -Session $session -ScriptBlock {
+        # Commande pour verrouiller l'ordinateur
+        rundll32.exe user32.dll,LockWorkStation
+    }
+
+    # Fermeture de la session PowerShell
+    Remove-PSSession $session
+}
