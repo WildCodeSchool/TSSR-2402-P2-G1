7fe5c9e08de8177c48ac8591be8050e091dc720a
Ahmedbenrebai <161340742+Ahmedbenrebai@users.noreply.github.com>
2024-04-16 12:04:20 +0200

    Create Shutdown.ps1

---
+++ Shutdown.ps1
@@ -0,0 +1,17 @@
+function Redemarrer-Client {
+    # Adresse IP ou nom d'hôte de l'ordinateur client
+    $clientAddress = "adresse_ip_ou_nom_hote"
+
+    # Nom d'utilisateur sur l'ordinateur client
+    $username = "utilisateur"
+
+    $session = New-PSSession -ComputerName $clientAddress -Credential $username
+
+    Invoke-Command -Session $session -ScriptBlock {
+        # Commande pour redémarrer l'ordinateur
+        Restart-Computer -Force
+    }
+
+    # Fermeture de la session PowerShell
+    Remove-PSSession $session
+}
