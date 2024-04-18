13fe23768589967a7a1bb7e69a3984bbcae9f0cd
cl_smoker <81968235+clsmoker88@users.noreply.github.com>
2024-04-18 08:32:41 +0200

    Update function.ps1

--- function.ps1
+++ function.ps1
@@ -1,17 +1,15 @@
-﻿function update_system()
-{
-    Update-Help
-}
+﻿#fonction pour avoir la ram total
 
 function ram_total()
 {
     Get-WmiObject Win32_PhysicalMemory | Format-Table capacity
 }
-
+#fonction pour avoir la version de l'OS
 function os_version()
 {
     Get-WmiObject Win32_OperatingSystem | Format-Table Version
 }
+#Fonction pour ajouter un utilisateur
 
 function add_user()
 {
@@ -32,13 +30,93 @@ function add_user()
     }
 
 }
+#fonction pour lister les utilisateurs local
 
 function list_user_local()
 {
-    Get-LocalUser
+    Get-LocalUser | Select-Object Name
 }
+#fonction pour afficher les services en cours
+
+function service_run()
+{
+    Get-Service | Where-Object {$_.Status -eq "running"} | Select-Object Name
+}
+#fonction pour afficher la liste des paquet installer
+
+function list_install()
+{
+   Get-AppxPackage | Select Name 
+}
+#fonction pour savoir a quelle groupe appartient l'utilisateur
+
+function user_group()
+{
+    $user = Read-Host "quelle utilisateur voulez vous cibler? "
+    if(Get-LocalUser -Name $user 2> $null)  
+    {
+        $group = net user $user  
+        write-host $group[22]
+    }
+    else
+    {
+        write-host "L'utilisateur n'existe pas"
+    }
+}
+
+function ChangePassword {
+    $nomUtilisateur = Read-Host "Nommez l'utilisateur"
+    $nouveauMotDePasse = Read-Host "Veuillez entrer le nouveaux mot de passe" -AsSecureString
+    }
+
+function last_change_password()
+{
+    $user = Read-Host "De quelle utilisateur voulez vous savoir a quelle date a eus lieux le dernier changement de mot passe"
+     if(Get-LocalUser -Name $user 2> $null)  
+    {
+        (Get-LocalUser -Name $user).PasswordLastSet
+    }
+    else
+    {
+        write-host "L'utilisateur n'existe pas"
+    }
+    
+}
+
+function info_disk {
+    # récupérer les lecteurs montés
+$drives = Get-PSDrive -PSProvider 'FileSystem'
+
+#inspecte chaque lecteur et afficher les détails
+foreach ($drive in $drives) {
+    if ($drive.Root -and (Test-Path $drive.Root)) {
+        try { # récupérer les informations pour chaque lecteur
+        $driveInfo = Get-Volume -DriveLetter $drive.Name[0]2>$null
+
+        # Afficher les informations
+        Write-Host "Nom du lecteur: $($drive.Name)"
+        Write-Host "Type de lecteur: $($driveInfo.FileSystemType)"
+        Write-Host "Espace libre: $($driveInfo.SizeRemaining / 1GB -as [int]) GB"
+        Write-Host "Espace total: $($driveInfo.Size / 1GB -as [int]) GB"
+
+    } 
+    catch {
+        Write-Host "Impossible de récupérer les informations pour le lecteur $($drive.Name)"
+    }
+} 
+else {
+    Write-Host "Le lecteur $($drive.Name) n'est pas accessible ou n'existe pas."
+    }
+}
+}
+
 #update_system
 #ram_total
 #os_version
 #add_user
-list_user_local
+#list_user_local
+#service_run
+#user_group
+#ChangePassword
+#last_change_password
+#info_disk
