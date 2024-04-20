87271c67e9d432f6e67b70b078ff9dae55d57a25
Lucapouilly <160779668+Lucapouilly@users.noreply.github.com>
2024-04-20 20:03:02 +0200

    Add files via upload

---
+++ Powershell/function Droitsdossier {.ps1
@@ -0,0 +1,10 @@
+function Droitsdossier {
+    
+    $droituser = Read-Host "de quel utilisateur verifier les doits sur un dossier "
+    $droitdoss = Read-Host "de quel dossier verifier les droits d'un utilisateur chemin complet "
+
+    Write-Output "Droits de l'utilisateur $droituser sur le dossier $droitdoss " >> $info_log
+    
+    Invoke-Command -ScriptBlock { param ($droitdoss,$droituser) Get-Acl -Path $droitdoss | Select-Object -ExpandProperty Access | Where-Object { $_.IdentityReference -match "$droituser" } } -argumentlist $droitdoss,$droituser -Session $session | Tee-Object $info_log -Append
+
+}
