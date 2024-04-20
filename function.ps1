a0d929f192c6884c361a9d6d6c66af8f14e57c0a
cl_smoker <81968235+clsmoker88@users.noreply.github.com>
2024-04-20 13:56:32 +0200

    Add files via upload

--- function.ps1
+++ function.ps1
@@ -2,25 +2,34 @@
 
 function ram_total()
 {
-     Invoke-Command -ScriptBlock { Get-WmiObject Win32_PhysicalMemory | Format-Table capacity } -Session $session
+     Invoke-Command -ScriptBlock { Get-WmiObject Win32_PhysicalMemory | Format-Table capacity } -Session $session | Tee-Object $info_log -Append
+
+     $message = "affichage de la ram total"
+     Write-Log $mesage
 }
 #fonction pour avoir la version de l'OS
 function os_version()
 {
-    Invoke-Command -ScriptBlock { Get-WmiObject Win32_OperatingSystem | Format-Table Version } -Session $session
+    Invoke-Command -ScriptBlock { Get-WmiObject Win32_OperatingSystem | Format-Table Version } -Session $session | Tee-Object $info_log -Append
+    $message = "affichage de la version de l'OS"
+    Write-Log $mesage
 }
 #Fonction pour ajouter un utilisateur
-#a revoir pour pas que ca ressemble a chat gpt
+
 function add_user {
     $username_user = Read-Host "Quel utilisateur voulez-vous ajouter?"
     if (Invoke-Command -ScriptBlock {param($username) Get-LocalUser -Name $username 2> $null} -ArgumentList $username_user -Session $session) {
         Write-Host "L'utilisateur existe déjà."
+        $message = "l'utilisateur $username_user ne peut pas etre creer car il exite deja"
+        Write-Log $mesage
     }
     else {
         $password_user = Read-Host -AsSecureString "Choisissez un mot de passe pour le compte"
         Invoke-Command -ScriptBlock {param($username, $password) New-LocalUser -Name $username -Password $password 2> $null} -ArgumentList $username_user, $password_user -Session $session
         if (Invoke-Command -ScriptBlock {param($username) Get-LocalUser -Name $username} -ArgumentList $username_user -Session $session) {
             Write-Host "L'utilisateur a bien été créé."
+            $message = "l'Utilisateur $username_user a été creer"
+            Write-Log $mesage
         }
     }
 }
@@ -28,33 +37,42 @@ function add_user {
 
 function list_user_local()
 {
-    Invoke-Command -ScriptBlock { Get-LocalUser | Select-Object Name } -Session $session
+    Invoke-Command -ScriptBlock { Get-LocalUser | Select-Object Name } -Session $session | Tee-Object $info_log -Append
+    $message = "affichage de la liste des utilisateurs local"
+    Write-Log $mesage
 }
 #fonction pour afficher les services en cours
 
 function service_run()
 {
-    Invoke-Command -ScriptBlock { Get-Service | Where-Object {$_.Status -eq "running"} | Select-Object Name }
+    Invoke-Command -ScriptBlock { Get-Service | Where-Object {$_.Status -eq "running"} | Select-Object Name } | Tee-Object $info_log -Append
+    $message = "affichage de services en cours d'execution"
+    Write-Log $mesage
 }
 #fonction pour afficher la liste des paquet installer
 
 function list_install()
 {
-   Invoke-Command -ScriptBlock { Get-AppxPackage | Select Name } 
+   Invoke-Command -ScriptBlock { Get-AppxPackage | Select Name } | Tee-Object $info_log -Append
+   $message = "affichage des paquets installé"
+    Write-Log $mesage
 }
 #fonction pour savoir a quelle groupe appartient l'utilisateur
-#fonction a revoir?
+
 function user_group()
 {
     $username_user = Read-Host "quelle utilisateur voulez vous cibler? "
     if (Invoke-Command -ScriptBlock { param($username) Get-LocalUser -Name $username 2> $null } -ArgumentList $username_user -Session $session)  
     {
-        Invoke-Command -ScriptBlock { param($username) net user $username | Where { $_ -eq $username} } -ArgumentList $username_user -Session $session  
-        #write-host $group[22]
+        Invoke-Command -ScriptBlock { param($username) (net user $username)[22,23] } -ArgumentList $username_user -Session $session | Tee-Object $info_log -Append
+        $message = "affichage a quelle groupe appaartient l'utilisateur"
+        Write-Log $mesage  
     }
     else
     {
         write-host "L'utilisateur n'existe pas"
+        $message = "impossible de savoir a quelle groupe appartient l'utilisateur"
+        Write-Log $mesage
     }
 }
 
@@ -63,18 +81,24 @@ function change_password()
     $nomUtilisateur = Read-Host "Nommez l'utilisateur: "
     $nouveauMotDePasse = Read-Host "Veuillez entrer le nouveaux mot de passe" -AsSecureString
     Invoke-Command -scriptBlock { param($nomUtilisateur, $nouveauMotDePasse) Get-LocalUser -Name $nomUtilisateur | Set-LocalUser -Password $nouveauMotDePasse } -ArgumentList $nomUtilisateur, $nouveauMotDePasse -Session $session
+    $message = "changement de mot de passe de l'utilisateur $nomUtilisateur"
+    Write-Log $mesage
 }
 
 function last_change_password()
 {
     $user = Read-Host "De quelle utilisateur voulez vous savoir a quelle date a eus lieux le dernier changement de mot passe"
-     if(Get-LocalUser -Name $user 2> $null)  
+     if(Invoke-Command -scriptBlock { param($user) Get-LocalUser -Name $user 2> $null } -ArgumentList $user -Session $session)  
     {
-        (Get-LocalUser -Name $user).PasswordLastSet
+        Invoke-Command -scriptBlock { param($user) (Get-LocalUser -Name $user).PasswordLastSet } -ArgumentList $user -Session $session | Tee-Object $info_log -Append
+        $message = "affichage de la derniere fois que $nomUtilisateur a changer de mot de passe"
+        Write-Log $mesage
     }
     else
     {
         write-host "L'utilisateur n'existe pas"
+         $message = "affichage de la derniere fois que $nomUtilisateur a changer de mot de passe ECHEC car l'utilisateur n'existe pas"
+         Write-Log $mesage
     }
     
 }
@@ -104,5 +128,6 @@ $info_log = "C:\Users\Administrator\Documents\info_wilder_$choix_users_$dates.tx
 #service_run
 #list_install
 #user_group
-change_password
+#change_password
 #last_change_password
+
