39cae67cf0edd8d84ae23f86264ae29b827544d3
Lucapouilly <160779668+Lucapouilly@users.noreply.github.com>
2024-04-20 20:02:23 +0200

    Delete Powershell directory

--- Powershell/function lecteur_list {.ps1
+++
@@ -1,6 +0,0 @@
-function lecteur_list {
-
-    Write-Output "Liste des lecteurs monter " >> $info_log
-    [System.IO.DriveInfo]::GetDrives() Select-Object Name,DriveType | Format-Table -AutoSize Session $session | Tee-Object $info_log -Append
-
-}
--- Powershell/function sesionactive {.ps1
+++
@@ -1,6 +0,0 @@
-function sesionactive {
-    
-    Write-Output "Liste des session actives" >> $info_log
-    Invoke-Command query session Session $seesion | Tee-Object $info_log -Append
-    
-}
